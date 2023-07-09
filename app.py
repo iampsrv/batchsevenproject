f6cb28c8ffa8679411ec430d9b3c330fb8a32cd0
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2023-07-09 21:36:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def delete_item_from_db(key):
     
 @app.route('/')
 def hello():
-    return "hello and welcome everyone"
+    return "Github initiated the trigger"
     
 @app.route('/items', methods=['GET'])
 def get_all_items():
